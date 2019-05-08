48eae1e17687f1182e5ae68067efd85f7b61ad4e
Aman-am <amanmehta015@gmail.com>
2019-05-08 19:44:39 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            roman: 'I'
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
 
+    handleChange(e) {
+        let dict = {
+            M:1000,
+            CM:900,
+            D:500,
+            CD:400,
+            C:100,
+            XC:90,
+            L:50,
+            XL:40,
+            X:10,
+            IX:9,
+            V:5,
+            IV:4,
+            I:1
+        };
+
+        let i,integer,roman = '';
+        integer = e.target.value;
+        for(i in dict){
+            while (integer >= dict[i]){
+                roman += i;
+                integer -= dict[i];
+            }
+        }
+        this.setState({roman});
+    }
+
+    render() {
+        return(
+            <div className="main">
+                <h1>Roman Number Generator</h1>
+                <label>
+                    {/*Number:*/}
+                    <input type="number" name="integer" min="1" onChange={this.handleChange} defaultValue="1"/>
+                </label>
+                <i className="fas fa-arrow-right"></i>
+                <label>
+                    {/*Roman:*/}
+                    <input type="text" name="roman" value={this.state.roman} disabled/>
+                </label>
+            </div>
+        );
+    }
+}
 export default App;
